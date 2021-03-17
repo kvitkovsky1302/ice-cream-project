183a7c966259a5fab10f21b9464ba6cbfa0e0029
Kvitkovsky Maxim <kvitkovsky1302@gmail.com>
2021-03-17 17:19:47 +0200

    fix mobile menu js

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -6,35 +6,39 @@
     const mobileLi2 = document.querySelector("[data-li2]");
     const mobileLi3 = document.querySelector("[data-li3]");
     const mobileLi4 = document.querySelector("[data-li4]");
+  const burger1 = document.querySelector(".burger-container span");
   
-  mobileMenuRef.addEventListener("mouseout", () => {
-    mobileMenuRef.classList.toggle("is-open");
-  })
-  // menuBtnRef.addEventListener("mouseout", () => {
+  // mobileMenuRef.addEventListener("mouseout", () => {
   //   mobileMenuRef.classList.toggle("is-open");
-  //   })
+  //     burger1.classList.toggle('active');
+  // })
   
   menuBtnRef.addEventListener("click", () => {
-      mobileMenuRef.classList.toggle("is-open");
+    mobileMenuRef.classList.toggle("is-open");
     })
 
   mobileBtnClose.addEventListener('click', () => {
-      mobileMenuRef.classList.toggle("is-open");
+    mobileMenuRef.classList.toggle("is-open");
+      burger1.classList.toggle('active');
     });
   
   mobileLi1.addEventListener("click", () => {
-    mobileMenuRef.classList.toggle("is-open"); 
+    mobileMenuRef.classList.toggle("is-open");
+    burger1.classList.toggle('active');
   })
 
   mobileLi2.addEventListener("click", () => {
-    mobileMenuRef.classList.toggle("is-open"); 
+    mobileMenuRef.classList.toggle("is-open");
+    burger1.classList.toggle('active');
   })
 
   mobileLi3.addEventListener("click", () => {
-    mobileMenuRef.classList.toggle("is-open"); 
+    mobileMenuRef.classList.toggle("is-open");
+    burger1.classList.toggle('active');
   })
 
   mobileLi4.addEventListener("click", () => {
-    mobileMenuRef.classList.toggle("is-open"); 
+    mobileMenuRef.classList.toggle("is-open");
+    burger1.classList.toggle('active');
     })
 })()
